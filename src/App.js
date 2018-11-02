df14affeb589d0352a18788d926e107f0c3f10bb
TUPOBO <1028461931@qq.com>
2018-11-02 20:22:45 +0800

    4-1 Using the Petfinder API Client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  componentDidMount() {
+    const promise = petfinder.breed.list({
+      animal: "dog"
+    });
+    promise.then(console.log, console.error);
+  }
+
   render() {
     return (
       <React.Fragment>
