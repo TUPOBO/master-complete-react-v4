36fdf989e951575cd93807cb4b20ddcba3deeb22
TUPOBO <1028461931@qq.com>
2018-11-04 16:24:47 +0800

    7-1 Handling Input State Change

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,12 +7,19 @@ class SearchParams extends Component {
     breed: ""
   };
 
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="search-params">
         <label htmlFor="location">
           Location
           <input
+            onChange={this.handleLocationChange}
             id="location"
             value={this.state.location}
             placeholder="Location"
