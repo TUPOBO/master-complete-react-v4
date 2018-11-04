75ae13b15b717ca48c9be4d8bc7fe733b5a50ab9
TUPOBO <1028461931@qq.com>
2018-11-04 16:33:39 +0800

    7-2 Adding a Select List with Options

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ANIMALS } from "petfinder-client";
 
 class SearchParams extends Component {
   state = {
@@ -13,6 +14,12 @@ class SearchParams extends Component {
     });
   };
 
+  handleAnimalChange = event => {
+    this.setState({
+      animal: event.target.value
+    });
+  };
+
   render() {
     return (
       <div className="search-params">
@@ -24,6 +31,22 @@ class SearchParams extends Component {
             value={this.state.location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </div>
     );
