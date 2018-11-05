65982e00bdba3d4ebc7750c668a28f22ecee3dff
TUPOBO <1028461931@qq.com>
2018-11-05 08:05:01 +0800

    8-1 Using the Provider

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,63 +1,7 @@
 import React, { Component } from "react";
 import pf, { ANIMALS } from "petfinder-client";
 
-// petfinder 客户端
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
 class SearchParams extends Component {
-  state = {
-    location: "Seattle, WA",
-    animal: "",
-    breed: "",
-    breeds: []
-  };
-
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  };
-
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value,
-        breed: ""
-      },
-      this.getBreeds
-    );
-  };
-
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed.list({ animal: this.state.animal }).then(data => {
-        if (
-          data.petfinder &&
-          data.petfinder.breeds &&
-          Array.isArray(data.petfinder.breeds.breed)
-        ) {
-          this.setState({
-            breeds: data.petfinder.breeds.breed
-          });
-        } else {
-          this.setState({ breeds: [] });
-        }
-      });
-    } else {
-      this.setState({
-        breeds: []
-      });
-    }
-  }
-
   render() {
     return (
       <div className="search-params">
