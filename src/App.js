73ef314ec24f3a6a14f31fa4005719b370da1860
TUPOBO <1028461931@qq.com>
2018-11-06 07:37:34 +0800

    11-0 Install Emotion & Extract a Module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { render } from "react-dom";
 import React from "react";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import pf from "petfinder-client";
 import { Provider } from "./SearchContext";
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import NavBar from "./NavBar";
 
 // petfinder 客户端
 const petfinder = pf({
@@ -75,14 +76,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
