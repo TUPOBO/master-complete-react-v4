55478dcf7659d52fba5c488ec06144befb6fd7ec
TUPOBO <1028461931@qq.com>
2018-11-06 17:05:56 +0800

    13-1 Root Reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+import location from "./location";
+
+/*
+  state = {location: 'Seattle, WA'}
+  action = {type: 'SET_LOCATION', payload: 'San Francisco, CA'}
+
+*/
+
+const rootReducer = function(state, action) {
+  switch (action.type) {
+    case "SET_LOCATION":
+      return location(state, action);
+  }
+};
+
+// export default combineReducers({
+//   location
+// });
