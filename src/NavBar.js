9b873647c8cbbaf07e1c419163d6cd02a84ee1e0
TUPOBO <1028461931@qq.com>
2018-11-06 08:11:06 +0800

    11-2 Reusing Styles with JavaScript

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "react-emotion";
-
+import colors from "./colors";
 const Container = styled("header")`
-  background-color: #333;
+  background-color: ${colors.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const NavBar = () => (
   <Container>
-    <Link to="/">Adopt Me!</Link>
-    <Link to="/search-params">
+    <NavLink to="/">Adopt Me!</NavLink>
+    <NavLink to="/search-params">
       <span aria-label="search" role="img">
         🔍
       </span>
-    </Link>
+    </NavLink>
   </Container>
 );
 
